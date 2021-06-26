36e82cd3d8e8d2f813623826444565ad29261f44
CreamyMilk <creamym254@gmail.com>
2021-06-26 22:42:58 +0300

    💪 Added realtime server connection and some location boiler plate

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,5 +1,8 @@
 class Constants{
   static const String boxName = 'theboxStore';
   static const String serverUrlStore = "urlStore";
-  static const String defaultUrl = "http://192.168.0.36:9000/data";
+   static const String socketIOStore = "IOStore";
+  static const String locationStore = "locationStore";
+  static const String defaultUrl = "http://192.168.0.36:9000";
+   static const String defaultsocketURL = "http://192.168.0.36:3000";
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,18 @@
+import 'dart:async';
 import 'dart:convert';
 import 'dart:io';
 
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:geolocator/geolocator.dart';
 import 'package:hive/hive.dart';
 import 'package:hive_flutter/hive_flutter.dart';
 import 'package:http/http.dart';
 
 import 'package:telephony/telephony.dart';
-import 'package:location/location.dart';
+
 import 'package:transp/constants.dart';
+import 'package:socket_io_client/socket_io_client.dart' as sio;
 
 dynamic backgroundMessageHandler(SmsMessage message) async {
   print("Background Message");
@@ -19,28 +22,21 @@ void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Hive.initFlutter();
   await Hive.openBox(Constants.boxName);
-  Location location =  Location();
-  location.enableBackgroundMode(enable: true);
-  PermissionStatus localperm = await location.hasPermission();
-  if(localperm == PermissionStatus.granted){
-    location.onLocationChanged.listen((LocationData currentLocation) {
-      //Send to Server if possible
-    });
-  }
   Telephony.instance.listenIncomingSms(
-		onNewMessage: (SmsMessage message) {
-		 showCupertinoDialog(
-      context: navigatorKey.currentContext!,
-      builder: (BuildContext context) {
-        return AlertDialog(
-            title: const Text('Foreground Message : '), content: Text('${message.body} ${message.address}'));
+      onNewMessage: (SmsMessage message) {
+        showCupertinoDialog(
+          context: navigatorKey.currentContext!,
+          builder: (BuildContext context) {
+            return AlertDialog(
+                title: const Text('Foreground Message : '),
+                content: Text('${message.body} ${message.address}'));
+          },
+        );
       },
-    );
-		},
-		onBackgroundMessage: backgroundMessageHandler
-	);
+      onBackgroundMessage: backgroundMessageHandler);
   runApp(const MyApp());
 }
+
 final navigatorKey = GlobalKey<NavigatorState>();
 
 class MyApp extends StatelessWidget {
@@ -67,122 +63,232 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   List<SmsMessage> messages = [];
+  late Timer _clock;
+  late sio.Socket socket;
+ 
 
   @override
-  void initState() {
+  void initState(){
     super.initState();
+    //askForLocationPermission();
+    connectToServer();
+  }
+
+  handleMessage(dynamic data) {
+    print(data);
+  }
+
+  Future askForLocationPermission() async {
+    bool serviceEnabled;
+    LocationPermission permission;
+    serviceEnabled = await Geolocator.isLocationServiceEnabled();
+    
+    if (!serviceEnabled) {
+      return "";
+    }
+
+    permission = await Geolocator.checkPermission();
+
+    if (permission == LocationPermission.denied) {
+      permission = await Geolocator.requestPermission();
+      if (permission == LocationPermission.denied) {
+        return "";
+      }
+    }
+
+    if (permission == LocationPermission.deniedForever) {
+      return "";
+    }
+    return "";
+  }
+
+  void connectToServer() {
+    try {
+      var box = Hive.box(Constants.boxName);
+      var socketURL = box.get(Constants.socketIOStore,
+          defaultValue: Constants.defaultsocketURL);
+      socket = sio.io(socketURL, <String, dynamic>{
+        'transports': ['websocket'],
+        'autoConnect': false,
+      });
+
+      socket.connect();
+
+      socket.on('connect', (_) {
+       
+        print('connect: ${socket.id}');
+      });
+      socket.on('message', handleMessage);
+      socket.on('disconnect', (_) {
+        print('disconnect');
+      });
+      socket.on('fromServer', (_) => print(_));
+    } catch (e) {
+      print(e.toString());
+    }
+  }
+
+  _sendSocketData() {
+    storeLocation();
+    var box = Hive.box(Constants.boxName);
+    var locationData =
+        box.get(Constants.locationStore, defaultValue: '{"locationdump":"{}"}');
+    socket.emit("message", locationData);
+  }
+
+  _startTimer() {
+    const oneSec = const Duration(seconds: 1);
+    _clock = Timer.periodic(oneSec, (Timer t) {
+      _sendSocketData();
+    });
+  }
+
+  @override
+  void dispose() {
+    _clock.cancel();
+    super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        floatingActionButton: const FloatingActionButton(
-            onPressed: sendMessagesToServer, child: Icon(Icons.send)),
-        appBar: AppBar(
-          actions:const [
-
-            IconButton(
-              onPressed: sendYourLocation,
-              icon: Icon(Icons.location_on),
-            ),
-            IconButton(
-              onPressed: changeURL,
-              icon: Icon(Icons.web_stories),
-            ),
-          ],
-          title: const Text("👀 List"),
-        ),
-        body: FutureBuilder(
-          future: fetchSMS(),
-          builder: (context, snapshot) {
-            if (snapshot.hasError) return const Text("Error");
-            return ListView.separated(
-                separatorBuilder: (context, index) => const Divider(
-                      color: Colors.black,
+      floatingActionButton: const FloatingActionButton(
+          onPressed: sendMessagesToServer, child: Icon(Icons.send)),
+      appBar: AppBar(
+        actions: [
+          IconButton(
+            onPressed: _startTimer,
+            icon: const Icon(Icons.bolt),
+          ),
+           IconButton(
+            onPressed: askForLocationPermission,
+            icon:const Icon(Icons.hail_rounded),
+          ),
+          const IconButton(
+            onPressed: changeURL,
+            icon: Icon(Icons.web_stories),
+          ),
+        ],
+        title: const Text("👀 List"),
+      ),
+      body: FutureBuilder(
+        future: fetchSMS(),
+        builder: (context, snapshot) {
+          if (snapshot.hasError) return const Text("Error");
+          return ListView.separated(
+              separatorBuilder: (context, index) => const Divider(
+                    color: Colors.black,
+                  ),
+              itemCount: messages.length,
+              itemBuilder: (context, index) {
+                return Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: ListTile(
+                    onTap: () {
+                      showCupertinoDialog(
+                        context: navigatorKey.currentContext!,
+                        builder: (BuildContext context) {
+                          return AlertDialog(
+                            title: const Text('::::   🦀    ::::'),
+                            content: Text(messages[index].body ?? ""),
+                          );
+                        },
+                      );
+                    },
+                    leading: const Icon(
+                      Icons.markunread,
+                      color: Colors.teal,
                     ),
-                itemCount: messages.length,
-                itemBuilder: (context, index) {
-                  return Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: ListTile(
-                      onTap:(){
-                        showCupertinoDialog(
-                          context: navigatorKey.currentContext!,
-                          builder: (BuildContext context) {
-                            return AlertDialog(
-                              title: const Text('::::   🦀    ::::'),
-                              content: Text(messages[index].body ?? ""),
-                              );
-                          },
-                        );
-                      },
-                      leading: const Icon(
-                        Icons.markunread,
-                        color: Colors.teal,
-                      ),
-                      title: Text(messages[index].address ?? ""),
-                      subtitle: Text(messages[index].body ?? "", maxLines: 2),
-                    ),
-                  );
-                });
-          },
-        ),
+                    title: Text(messages[index].address ?? ""),
+                    subtitle: Text(messages[index].body ?? "", maxLines: 2),
+                  ),
+                );
+              });
+        },
+      ),
     );
   }
 
   Future<List<SmsMessage>> fetchSMS() async {
-    if(Platform.isAndroid){
-    messages = await Telephony.instance.getInboxSms(
-        filter: SmsFilter.where(SmsColumn.ADDRESS)
-            .equals("MPESA"));
-    }else{
+    List<SmsMessage> _messages = [];
+    if (Platform.isAndroid) {
+      _messages = await Telephony.instance.getInboxSms(
+          filter: SmsFilter.where(SmsColumn.ADDRESS).equals("MPESA"));
+      messages = _messages;
+    } else {
       messages = [];
     }
-    return messages;
+    return _messages;
   }
 }
 
 void changeURL() {
   var box = Hive.box(Constants.boxName);
-  var _controler = TextEditingController();
-  _controler.value = TextEditingValue(text:box.get(Constants.serverUrlStore,defaultValue:Constants.defaultUrl));
+  var _remoteurlController = TextEditingController();
+  // ignore: non_constant_identifier_names
+  var _IOController = TextEditingController();
+
+  _IOController.value = TextEditingValue(
+      text: box.get(Constants.socketIOStore,
+          defaultValue: Constants.defaultsocketURL));
+
+  _remoteurlController.value = TextEditingValue(
+      text: box.get(Constants.serverUrlStore,
+          defaultValue: Constants.defaultUrl));
+
   showModalBottomSheet<void>(
-    isScrollControlled:true,
-    context:navigatorKey.currentContext!,
-    builder: (BuildContext ctx){
-      return SingleChildScrollView(
-        child:Padding(
-        padding:
-            EdgeInsets.only(bottom: MediaQuery.of(ctx).viewInsets.bottom,left: 30,right:30),
-        child:SizedBox(
-          height: 100,
-          child:Center(
-          child:TextField(
-            decoration: const InputDecoration(
-                  border: OutlineInputBorder(gapPadding: 2),
-                  labelText: 'Enter Remote Server URL',
-                  hintText: 'Hope it supports JSON',
-                ),
-            controller:_controler,
-           
-            onChanged: (String s){
-               box.put(Constants.serverUrlStore,s);
-            },
-          )),
-        ),),
-      );
-    }
-    );
+      isScrollControlled: true,
+      context: navigatorKey.currentContext!,
+      builder: (BuildContext ctx) {
+        return SingleChildScrollView(
+          child: Padding(
+            padding: EdgeInsets.only(
+                bottom: MediaQuery.of(ctx).viewInsets.bottom,
+                left: 30,
+                right: 30),
+            child: SizedBox(
+              height: 150,
+              child: Center(
+                  child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  TextField(
+                    decoration: const InputDecoration(
+                      border: OutlineInputBorder(gapPadding: 2),
+                      labelText: 'Enter Remote Server URL',
+                      hintText: 'Hope it supports JSON',
+                    ),
+                    controller: _remoteurlController,
+                    onChanged: (String s) {
+                      box.put(Constants.serverUrlStore, s);
+                    },
+                  ),
+                  const SizedBox(height: 10),
+                  TextField(
+                    decoration: const InputDecoration(
+                      border: OutlineInputBorder(gapPadding: 2),
+                      labelText: '⚡ Socket URL',
+                      hintText: 'socketIo is the way',
+                    ),
+                    controller: _IOController,
+                    onChanged: (String s) {
+                      box.put(Constants.socketIOStore, s);
+                    },
+                  ),
+                ],
+              )),
+            ),
+          ),
+        );
+      });
 }
 
 void sendMessagesToServer() async {
-  //get messages
   List<Map<String, dynamic>> dataBlob = [];
 
   final messagesDump = await Telephony.instance
       .getInboxSms(filter: SmsFilter.where(SmsColumn.ADDRESS).equals("MPESA"));
   for (SmsMessage message in messagesDump) {
-   
     Map<String, String?> messageDestructure = {
       "subscriptionid": message.subscriptionId.toString(),
       "messageTimestamp": message.date.toString(),
@@ -193,11 +299,12 @@ void sendMessagesToServer() async {
     };
     dataBlob.add(messageDestructure);
   }
-  
+
   try {
     var box = Hive.box(Constants.boxName);
-    var serverUrl = box.get(Constants.serverUrlStore,defaultValue:Constants.defaultUrl);
-    var response = await post(Uri.parse(serverUrl+"/data"),
+    var serverUrl =
+        box.get(Constants.serverUrlStore, defaultValue: Constants.defaultUrl);
+    var response = await post(Uri.parse(serverUrl + "/data"),
         headers: {
           "Accept": "application/json",
           "Content-type": "application/json",
@@ -214,7 +321,6 @@ void sendMessagesToServer() async {
       },
     );
   } catch (error) {
-;
     showCupertinoDialog(
       context: navigatorKey.currentContext!,
       builder: (BuildContext context) {
@@ -225,49 +331,21 @@ void sendMessagesToServer() async {
   }
 }
 
-
-
 void sendYourLocation() async {
-  Location location =  Location();
-  bool _serviceEnabled;
-  PermissionStatus _permissionGranted;
-  LocationData _l;
-  _serviceEnabled = await location.serviceEnabled();
-  if (!_serviceEnabled) {
-    _serviceEnabled = await location.requestService();
-    if (!_serviceEnabled) {
-      return;
-    }
-  }
-  _permissionGranted = await location.hasPermission();
-  if (_permissionGranted == PermissionStatus.denied) {
-    _permissionGranted = await location.requestPermission();
-    if (_permissionGranted != PermissionStatus.granted) {
-      return;
-    }
-  }
-
-  _l = await location.getLocation(); 
-   
-    Map<String, double?> locationRest = {
-      "lat" : _l.latitude ?? 999,
-      "lon": _l.longitude ?? 999,
-      "alt": _l.altitude  ?? 999,
-      "speed": _l.speed ?? 999,
-      "accuracy" : _l.accuracy ??999,
-      "saccuracy": _l.speedAccuracy ?? 999,
-    };
-
-  
   try {
+    storeLocation();
     var box = Hive.box(Constants.boxName);
-    var serverUrl = box.get(Constants.serverUrlStore,defaultValue:Constants.defaultUrl);
-    var response = await post(Uri.parse(serverUrl+"/location"),
+
+    var serverUrl =
+        box.get(Constants.serverUrlStore, defaultValue: Constants.defaultUrl);
+    var locationData =
+        box.get(Constants.locationStore, defaultValue: '{"locationdump":"{}"}');
+    var response = await post(Uri.parse(serverUrl + "/location"),
         headers: {
           "Accept": "application/json",
           "Content-type": "application/json",
         },
-        body: jsonEncode({"localdump": locationRest}));
+        body: locationData);
     var jsonresponse = json.decode(response.body);
 
     showCupertinoDialog(
@@ -279,7 +357,6 @@ void sendYourLocation() async {
       },
     );
   } catch (error) {
-   
     showCupertinoDialog(
       context: navigatorKey.currentContext!,
       builder: (BuildContext context) {
@@ -288,4 +365,35 @@ void sendYourLocation() async {
       },
     );
   }
+}
+
+void storeLocation() async {
+  var box = Hive.box(Constants.boxName);
+  bool serviceEnabled;
+  LocationPermission permission;
+  serviceEnabled = await Geolocator.isLocationServiceEnabled();
+  if (!serviceEnabled) {
+    return Future.error('Location services are disabled.');
+  }
+  permission = await Geolocator.checkPermission();
+  if (permission == LocationPermission.denied) {
+    return Future.error('Location permissions are denied');
+  }
+
+  if (permission == LocationPermission.deniedForever) {
+    return Future.error(
+        'Location permissions are permanently denied, we cannot request permissions.');
+  }
+
+  Position _l = await Geolocator.getCurrentPosition();
+  Map<String, double?> locationRest = {
+    "lat": _l.latitude,
+    "lon": _l.longitude,
+    "alt": _l.altitude,
+    "speed": _l.speed,
+    "accuracy": _l.accuracy,
+    "saccuracy": _l.speedAccuracy,
+    "comapss": _l.heading,
+  };
+  box.put(Constants.locationStore, jsonEncode({"locationdump": locationRest}));
 }
